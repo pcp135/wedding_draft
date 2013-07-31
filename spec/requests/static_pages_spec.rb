fff82f5a1b17f7d781db7f7a6e78bd5a9104e7b4
Phil <pcp135@gmail.com>
2013-07-31 21:19:30 +0200

    added tests for the program page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,27 @@ describe "StaticPages" do
       expect(page).to have_content('Welcome to our Wedding Website')
     end
 
-
   end
+  
+  describe "Program Page" do
+
+    it "should have a title 'Program'" do
+      visit '/static_pages/program'
+      expect(page).to have_content('Program')
+    end
+    it "should mention Friday" do
+      visit '/static_pages/program'
+      expect(page).to have_content('Friday')
+    end
+    it "should mention Saturday" do
+      visit '/static_pages/program'
+      expect(page).to have_content('Saturday')
+    end
+    it "should mention Sunday" do
+      visit '/static_pages/program'
+      expect(page).to have_content('Sunday')
+    end
+    
+  end
+      
 end
