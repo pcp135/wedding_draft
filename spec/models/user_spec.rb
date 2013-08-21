f6deaf09fa9df3ea7706bdacfbe629b969faa490
Phil <pcp135@gmail.com>
2013-08-21 14:43:35 +0200

    added first password validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com")}
+  before { @user = User.new(name: "Example User", email: "user@example.com",
+    password: "foobar", password_confirmation: "foobar")}
   subject { @user}
   it { should respond_to(:name)}
   it { should respond_to(:email)}
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
   it { should be_valid}
   describe "when name is not present" do
     before { @user.name = " "}
@@ -16,6 +20,17 @@ describe User do
   end
   describe "when name is too long" do
     before { @user.name = "a"*51 }
+    it { should_not be_valid }
+  end
+  describe "when password is not present" do
+    before { 
+      @user.password = " " 
+      @user.password_confirmation = " " 
+      }
+    it { should_not be_valid }
+  end
+  describe "when password doesn't match confirmation " do
+    before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
   end
   describe "when email format is invalid " do
