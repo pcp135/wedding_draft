49b286fd4daadd46d691cca5e996fd2879d7485b
Phil <phil@parsons.uk.com>
2013-08-21 18:52:07 +0200

    added basic cookie and session management

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,15 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      flash[:success] = "Welcome back "+ user.name
+      redirect_to root_path
+    else
+      flash.now[:error] = "Invalid email/password combination"
+      render 'new'
+    end
   end
   
   def destroy
