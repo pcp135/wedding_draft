e38f3a3f7cf6ac052df866249f2cd15998382eb8
Phil <pcp135@gmail.com>
2013-08-21 14:58:29 +0200

    finished password validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
   it { should be_valid}
   describe "when name is not present" do
     before { @user.name = " "}
@@ -23,15 +24,28 @@ describe User do
     it { should_not be_valid }
   end
   describe "when password is not present" do
-    before { 
-      @user.password = " " 
-      @user.password_confirmation = " " 
-      }
+    before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
   end
   describe "when password doesn't match confirmation " do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
+  end
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email)}
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid")}
+      it { should_not eq user_for_invalid_password}
+      specify { expect(user_for_invalid_password).to be_false }
+    end
+  end
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a"*5 }
+    it {should_not be_valid }
   end
   describe "when email format is invalid " do
     it "should be invalid" do
