cd10b806c2c130cacf9bc5495c576bff3219d63a
Phil <phil@parsons.uk.com>
2013-08-24 15:54:51 +0200

    modified path routing to work with locale embedded in the url as well

--- spec/requests/authenticate_pages_spec.rb
+++ spec/requests/authenticate_pages_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "Authentication" do
   subject { page}
   describe "signin page" do
-    before {visit signin_path }
+    before {visit signin_path(locale: :en ) }
     it {should have_content('Sign in')}
     it {should have_title('Sign in')}
   end
   describe "signin" do
-    before {visit signin_path }
+    before {visit signin_path(locale: :en ) }
     describe "with invalid information" do
       before {click_button "Sign in"}
       it {should have_title('Sign in')}
@@ -41,7 +41,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user)}
       describe "in the static pages" do
         describe "visiting the program page" do
-          before { visit program_path }
+          before { visit program_path(locale: :en)}
           it {should have_title('Sign in')}
           describe "after signing in" do
             before do 
