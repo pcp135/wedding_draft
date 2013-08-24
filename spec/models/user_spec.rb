b5e898306f2325876708c7f1c57bc7087bc73c41
Phil <phil@parsons.uk.com>
2013-08-24 17:19:46 +0200

    started writing tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ describe User do
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate) }
+  it { should respond_to(:admin) }
   it { should be_valid}
   describe "when name is not present" do
     before { @user.name = " "}
