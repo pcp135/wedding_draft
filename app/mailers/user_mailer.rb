591b038c028956302eb21771424350e8fb37b352
Phil <phil@parsons.uk.com>
2013-08-24 16:55:34 +0200

    fixed bug in password reset path given change of locale routing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "wedding@parsons-hense.info"
+  default from: "Steffi & Phil <wedding@parsons-hense.info>"
 
   def password_reset(user)
     @user = user
